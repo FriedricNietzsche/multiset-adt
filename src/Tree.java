f27da44e935aa70035569c0e2decc78e351d1b49
Krisvir  Aujla <krisviraujla@Krisvirs-MacBook-Air.local>
2025-05-15 19:45:27 -0400

    setup tree class

--- src/Tree.java
+++ src/Tree.java
@@ -1,3 +1,44 @@
+import javax.lang.model.type.NullType;
+import java.util.ArrayList;
 public class Tree {
-    // TODO complete this Tree class to replicate the implementation from the Tree class in adts.py
+    private ArrayList<Tree> subtrees;
+    private Object value;
+    public Tree(){
+        this.value = null;
+        this.subtrees = new ArrayList<>();
+    }
+    public Tree(Object value){
+        this.value = value;
+        this.subtrees = new ArrayList<>();
+    }
+    public Tree(Object value, ArrayList<Tree> subtrees){
+        this.value = value;
+        this.subtrees = subtrees;
+    }
+
+    public void insert(Object value){
+        // TODO: Finish method
+    }
+    public void remove(Object value){
+        // TODO: Finish method
+    }
+    public boolean contains(Object value){
+        // TODO: Finish method
+    }
+    public boolean isEmpty(Object value){
+        // TODO: Finish method
+    }
+
+    public int count(Object value){
+        // TODO: Finish method
+    }
+
+    public void size(Object value){
+        // TODO: Finish method
+    }
+
+    public String toString(){
+        // TODO: Finish method
+    }
+
 }
